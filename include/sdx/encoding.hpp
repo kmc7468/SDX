d190f42152376f37d94f8f4634d7c1a6051200ea
static <kmc7468@naver.com>
2018-01-14 15:07:14 +0900

    Add encoding class
    This class is the parent class of classes that represent the encoding of the text.

---
+++ include/sdx/encoding.hpp
@@ -0,0 +1,34 @@
+#ifndef SDX_HEADER_ENCODING_HPP
+#define SDX_HEADER_ENCODING_HPP
+
+#include <cstdint>
+#include <memory>
+
+namespace sdx
+{
+	class encoding
+	{
+	public:
+		using ptr = std::shared_ptr<encoding>;
+
+	public:
+		encoding() = delete;
+		encoding(const encoding& encoding) = delete;
+		encoding(encoding&& encoding) noexcept = delete;
+		virtual ~encoding() = default;
+
+	public:
+		encoding& operator=(const encoding& encoding) = delete;
+		encoding& operator=(encoding&& encoding) noexcept = delete;
+		bool operator==(const encoding& encoding) const noexcept;
+		bool operator!=(const encoding& encoding) const noexcept;
+
+	public:
+		bool equal(const encoding& encoding) const noexcept;
+
+	public:
+		virtual std::uint16_t codepage() const noexcept = 0;
+	};
+}
+
+#endif
