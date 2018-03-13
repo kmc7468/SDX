e2ac29a5900fa91c5d4066da1e804e9e16d74ffd
static <kmc7468@naver.com>
2018-03-13 20:59:55 +0900

    Add math::numeric_allocator::at method
    It will access the array inside the class. We also added sdx::math::numeric_allocator::operator[] operator. It almost the same as the sdx::math::numeric_allocator::at method, but it doesn't check the range of index.
    
    sdx::math::numeric_allocator<typename, 0> class has been removed. We will develop it later.

--- include/sdx/math/numeric_allocator.hpp
+++ include/sdx/math/numeric_allocator.hpp
@@ -1,7 +1,9 @@
 #ifndef SDX_HEADER_MATH_NUMERIC_ALLOCATOR_HPP
 #define SDX_HEADER_MATH_NUMERIC_ALLOCATOR_HPP
 
+#include <array>
 #include <cstddef>
+#include <stdexcept>
 
 namespace sdx
 {
@@ -18,42 +20,55 @@ namespace sdx
 			static constexpr bool is_dynamic = false;
 
 		public:
-			bool is_allocated() const noexcept
+			const block_type& operator[](std::size_t index) const noexcept
+			{
+				return data_[index];
+			}
+			block_type& operator[](std::size_t index) noexcept
+			{
+				return data_[index];
+			}
+
+		public:
+			const block_type& at(std::size_t index) const
+			{
+				if (index >= size_constant)
+					throw std::out_of_range("'index' is out of range.");
+
+				return operator[](index);
+			}
+			block_type& at(std::size_t index)
+			{
+				if (index >= size_constant)
+					throw std::out_of_range("'index' is out of range.");
+
+				return operator[](index);
+			}
+
+		public:
+			bool allocated() const noexcept
 			{
 				return true;
 			}
+			bool reallocatable() const noexcept
+			{
+				return false;
+			}
+
+		public:
 			std::size_t size() const noexcept
 			{
-				return Size_;
+				return size_constant;
 			}
 
 		private:
-			Block_ data_[Size_];
+			Block_ data_[size_constant];
 		};
 
 		template<typename Block_>
 		class numeric_allocator<Block_, 0>
 		{
-		public:
-			using block_type = Block_;
-
-		public:
-			static constexpr std::size_t size_constant = 0;
-			static constexpr bool is_dynamic = true;
-
-		public:
-			bool is_allocated() const noexcept
-			{
-				return data_ != nullptr;
-			}
-			std::size_t size() const noexcept
-			{
-				return size_;
-			}
-
-		private:
-			Block_* data_ = nullptr;
-			std::size_t size_ = 0;
+			// TODO
 		};
 	}
 }
