d8481fb9cd2016b27ad6fa5c99e3e45a380242b3
static <kmc7468@naver.com>
2018-03-17 20:20:46 +0900

    Add math::numeric_allocator::begin, cbegin, end, and cend methods
    These methods are similar to begin, cbegin, end, and cend in the STL container.

--- include/sdx/math/numeric_allocator.hpp
+++ include/sdx/math/numeric_allocator.hpp
@@ -173,6 +173,126 @@ namespace sdx
 
 				return reinterpret_cast<Block_*>(reinterpret_cast<std::uintptr_t>(iterator.data()) + rhs);
 			}
+		
+			template<typename Block_>
+			class numeric_allocator_const_iterator_
+			{
+			public:
+				using difference_type = std::ptrdiff_t;
+				using value_type = Block_;
+				using pointer = const value_type*;
+				using reference = const value_type&;
+				using iterator_category = std::random_access_iterator_tag;
+
+			private:
+				using my_ = numeric_allocator_const_iterator_<Block_>;
+				using non_const_pointer_ = value_type*;
+				using non_const_reference_ = value_type&;
+
+			public:
+				numeric_allocator_const_iterator_() = default;
+				numeric_allocator_const_iterator_(const value_type* data) noexcept
+					: data_(data)
+				{}
+				numeric_allocator_const_iterator_(const my_& iterator) noexcept
+					: data_(iterator.data_)
+				{}
+				numeric_allocator_const_iterator_(my_&& iterator) noexcept
+					: data_(iterator.data_)
+				{}
+				~numeric_allocator_const_iterator_() = default;
+
+			public:
+				my_& operator=(const my_& iterator) noexcept = delete;
+				my_& operator=(my_&& iterator) noexcept = delete;
+				bool operator==(const my_& iterator) const noexcept
+				{
+					return data_ == iterator.data_;
+				}
+				bool operator!=(const my_& iterator) const noexcept
+				{
+					return data_ != iterator.data_;
+				}
+				bool operator>(const my_& iterator) const noexcept
+				{
+					return data_ > iterator.data_;
+				}
+				bool operator>=(const my_& iterator) const noexcept
+				{
+					return data_ >= iterator.data_;
+				}
+				bool operator<(const my_& iterator) const noexcept
+				{
+					return data < iterator.data_;
+				}
+				bool operator<=(const my_& iterator) const noexcept
+				{
+					return data <= iterator.data_;
+				}
+				my_ operator+(difference_type value) const noexcept
+				{
+					difference_type rhs = value * sizeof(value_type);
+
+					return reinterpret_cast<value_type*>(reinterpret_cast<std::uintptr_t>(data_) + rhs);
+				}
+				my_ operator-(const my_& iterator) const noexcept
+				{
+					return (reinterpret_cast<std::uintptr_t>(data_) - reinterpret_cast<std::uintptr_t>(iterator.data_)) / sizeof(value_type);
+				}
+				my_ operator-(difference_type value) const noexcept
+				{
+					difference_type rhs = value * sizeof(value_type);
+
+					return reinterpret_cast<value_type*>(reinterpret_cast<std::uintptr_t>(data_) + rhs);
+				}
+				my_& operator+=(difference_type value) noexcept = delete;
+				my_& operator-=(difference_type value) noexcept = delete;
+				reference operator[](difference_type index) const noexcept
+				{
+					return data_[index];
+				}
+				non_const_reference_ operator[](difference_type index) noexcept = delete;
+				pointer operator->() const noexcept
+				{
+					return data_;
+				}
+				non_const_pointer_ operator->() noexcept = delete;
+				my_& operator++() noexcept = delete;
+				my_ operator++(int) const noexcept
+				{
+					my_ result = *this;
+					return ++result;
+				}
+				my_& operator--() noexcept = delete;
+				my_ operator--(int) const noexcept
+				{
+					my_ result = *this;
+					return --result;
+				}
+				reference operator*() const noexcept
+				{
+					return *data_;
+				}
+				non_const_reference_ operator*() noexcept = delete;
+
+			public:
+				const value_type* data() const noexcept
+				{
+					return data_;
+				}
+
+			private:
+				const value_type* data_ = nullptr;
+			};
+
+			template<typename Block_>
+			numeric_allocator_const_iterator_<Block_> operator+(typename numeric_allocator_const_iterator_<Block_>::difference_type value,
+				const numeric_allocator_const_iterator_<Block_>& iterator) noexcept
+			{
+				decltype(value) rhs = value * sizeof(Block_);
+
+				return reinterpret_cast<Block_*>(reinterpret_cast<std::uintptr_t>(iterator.data()) + rhs);
+			}
 		}
 	}
 
@@ -184,7 +304,7 @@ namespace sdx
 		public:
 			using value_type = Block_;
 			using iterator = details::math::numeric_allocator_iterator_<Block_>;
-			using const_iterator = const iterator;
+			using const_iterator = details::math::numeric_allocator_const_iterator_<Block_>;
 
 		private:
 			using my_ = numeric_allocator<Block_, Size_>;
@@ -272,6 +392,30 @@ namespace sdx
 			bool reallocatable() const noexcept
 			{
 				return false;
+			}
+			const_iterator begin() const noexcept
+			{
+				return &data_[0];
+			}
+			iterator begin() noexcept
+			{
+				return &data_[0];
+			}
+			const_iterator cbegin() const noexcept
+			{
+				return &data_[0];
+			}
+			const_iterator end() const noexcept
+			{
+				return &data_[size_constant];
+			}
+			iterator end() noexcept
+			{
+				return &data_[size_constant];
+			}
+			const_iterator cend() const noexcept
+			{
+				return &data_[size_constant];
 			}
 
 		public:
